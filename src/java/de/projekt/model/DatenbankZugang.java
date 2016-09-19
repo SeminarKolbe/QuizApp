8e9de1a96e7ecb83a8fcf9fcaf5c1e40a7d86c3f
Marin <Marin@10.18.149.147>
2016-09-19 13:47:55 +0200

    test

--- src/java/de/projekt/model/DatenbankZugang.java
+++ src/java/de/projekt/model/DatenbankZugang.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package de.projekt.model;
+package de.projekt.model; //TEstttttt
 
 import java.sql.*;
 import java.util.ArrayList;
